32600e471827b81d381713cfe6b1bf00cfef37bb
mainkeys <dymainkeys@gmail.com>
2024-02-27 23:38:56 +0800

    Refactor

--- src/utils/date.js
+++
@@ -1,8 +0,0 @@
-import dayjs from 'dayjs'
-export const dateFormat = (val, format = 'YYYY-MM-DD') => {
-  if (isNaN(val)) {
-    return val
-  }
-  val = parseInt(val)
-  return dayjs(val).format(format)
-}
