dae6e71c43d5a83df9a60208f0f98740b7aab636
mainkeys <dymainkeys@gmail.com>
2024-02-28 00:27:33 +0800

    cz + husky

---
+++ mks-admin/src/main.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: mainkeys
+ * @Date: 2024-02-27 23:45:56
+ * @LastEditors: mainkeys dymainkeys@gmail.com
+ * @LastEditTime: 2024-02-28 00:17:39
+ * @FilePath: \mks_admin\mks-admin\src\main.js
+ * @Description: 项目入口文件
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
