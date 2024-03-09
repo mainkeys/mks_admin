07e6ee613281b06bcf50a80ccdf1e65d346e21d0
mainkeys <dymainkeys@gmail.com>
2024-03-09 13:25:50 +0800

    navbar

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,7 +1,15 @@
+/*
+ * @Author: mainkeys
+ * @Date: 2024-03-08 22:47:23
+ * @LastEditors: mainkeys dymainkeys@gmail.com
+ * @LastEditTime: 2024-03-08 23:11:14
+ * @FilePath: \mks_admin\src\store\getters.js
+ * @Description: getters
+ */
 
 const getters = {
   token: state => state.user.token,
-  // userInfo: state => state.user.userInfo,
+  userInfo: state => state.user.userInfo,
   /**
    * @returns true 表示已存在用户信息
    */
